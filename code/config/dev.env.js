027f2d5d9bd1962038d02b2ecc96316a1ff9761e
zhanggongze <1812055216@qq.com>
2021-01-05 09:26:34 +0800

    域名修改

--- code/config/dev.env.js
+++ code/config/dev.env.js
@@ -2,7 +2,11 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
+// module.exports = merge(prodEnv, {
+//   NODE_ENV: '"development"',
+//   api:"https://qcloud.services.dev.hygeian.com.cn/services/report/api/v1/"
+// })
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  api:"https://qcloud.services.dev.hygeian.com.cn/services/report/api/v1/"
+  api:"https://qcloud.services.pro.hygeian.com.cn/services/report/api/v1/"
 })
